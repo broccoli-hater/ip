686e958e18d549ba150f86336a2e5d08ed9aa4cd
broccoli-hater <ihatebroccoli404@gmail.com>
2025-01-24 06:19:56 +0800

    added personality and echo

--- src/main/java/SirTalksALot.java
+++ src/main/java/SirTalksALot.java
@@ -1,13 +1,37 @@
+import java.util.Scanner;
+
 public class SirTalksALot {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("____________________________________________________________");
-        String welcome = "Hello! I'm Sir Talks-a-Lot"
-                       + "What can I do for you?";
-        System.out.println(welcome);
-        System.out.println("____________________________________________________________");
-        String bye = "Bye. Hope to see you again soon!";
-        System.out.println(bye);
+        greeting();
         System.out.println("____________________________________________________________");
 
+        String input = scanner.nextLine();
+        while (!input.equals("bye")) {
+            System.out.println("____________________________________________________________");
+            System.out.println(input);
+            System.out.println("____________________________________________________________");
+            input = scanner.nextLine();
+        }
+
+        farewell();
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void greeting() {
+        String greeting = "Greetings! It is I, Sir Talks-A-Lot. \n"
+                + "Ah, so you wish to converse with one such as myself? \n"
+                + "A noble knight, sworn to honor and valor, holder of great wisdom and unyielding strength? \n"
+                + "Very well, I shall indulge thee in thy request. \n"
+                + "Speak now, peasant, What dost thou seek from a knight of my stature?";
+        System.out.println(greeting);
+    }
+    public static void farewell() {
+        String bye = "Hah! A quick departure, is it? Very well, then. \n"
+                + "But should you wish to return and bask in the glory of my knightly presence, you need only call. \n"
+                + "Farewell for now.";
+        System.out.println(bye);
     }
 }
