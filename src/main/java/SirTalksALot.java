9073671d3edc3ae5d94c3653483dce4a293c75e3
broccoli-hater <ihatebroccoli404@gmail.com>
2025-01-24 06:00:45 +0800

    rename, greet, exit

---
+++ src/main/java/SirTalksALot.java
@@ -0,0 +1,13 @@
+public class SirTalksALot {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
+        String welcome = "Hello! I'm Sir Talks-a-Lot"
+                       + "What can I do for you?";
+        System.out.println(welcome);
+        System.out.println("____________________________________________________________");
+        String bye = "Bye. Hope to see you again soon!";
+        System.out.println(bye);
+        System.out.println("____________________________________________________________");
+
+    }
+}
