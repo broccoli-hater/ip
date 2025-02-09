109a1d8b877d2a3955a1b495e956363f00e37395
broccoli-hater <ihatebroccoli404@gmail.com>
2025-02-09 22:46:56 +0800

    Add basic GUI

--- src/main/java/SirTalksALot.java
+++ src/main/java/SirTalksALot.java
@@ -12,10 +12,11 @@ import util.Ui;
  * It supports delete, marking, unmarking and saving functions.
  */
 public class SirTalksALot {
-    private Storage storage;
-    private Ui ui = new Ui();
-    private Parser parser = new Parser();
-    private TaskList taskList;
+    private final Storage storage;
+    private final Ui ui = new Ui();
+    private final Parser parser = new Parser();
+    private final TaskList taskList;
+    private String commandType;
 
     /**
      * Constructs a SirTalksALot object with the specified file path for data storage.
@@ -46,6 +47,10 @@ public class SirTalksALot {
 
             Command command = parser.parse(input);
             command.execute(taskList, ui, storage);
+
+            commandType = command.getClass().getSimpleName();
+            System.out.println(commandType);
+
             if (command instanceof ExitCommand) {
                 isExit = true;
             }
@@ -56,11 +61,25 @@ public class SirTalksALot {
     }
 
     /**
-     * The entry point of the SirTalksALot application.
-     *
-     * @param args Command-line arguments (not used).
+     * Generates a response for the user's chat message.
      */
-    public static void main(String[] args) {
-        new SirTalksALot("data/sirtalksalot.txt").run();
+    public String getResponse(String input) {
+        Command command = parser.parse(input);
+        command.execute(taskList, ui, storage);
+        commandType = command.getClass().getSimpleName();
+        return commandType;
     }
+
+    public String getCommandType() {
+        return commandType;
+    }
+
+//    /**
+//     * The entry point of the SirTalksALot application.
+//     *
+//     * @param args Command-line arguments (not used).
+//     */
+//    public static void main(String[] args) {
+//        new SirTalksALot("data/sirtalksalot.txt").run();
+//    }
 }
